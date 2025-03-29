27cf1d9e21eaf853f42b0478d595e7860dfbb391
kimmk1533 <55845465+kimmk1533@users.noreply.github.com>
2025-03-29 20:08:53 +0900

    OperatorManager 템플릿 업데이트

--- Assets/Sciprts/MikangMark/OperatorManager.cs
+++ Assets/Sciprts/MikangMark/OperatorManager.cs
@@ -65,34 +65,36 @@ namespace AvantGardeMaker.MikangMark
 		#region 유니티 콜백 함수
 		#endregion
 
+		#region 초기화 & 마무리화 함수
 		/// <summary>
 		/// 초기화 함수 (Init Scene 진입 시, 즉 게임 실행 시 호출)
 		/// </summary>
-		public virtual void Initialize()
+		public override void Initialize()
 		{
 			base.Initialize();
 		}
 		/// <summary>
 		/// 마무리화 함수 (게임 종료 시 호출)
 		/// </summary>
-		public virtual void Finallize()
+		public override void Finallize()
 		{
 			base.Finallize();
 		}
 
 		/// <summary>
-		/// 게임 초기화 함수 (Game Scene 진입 시 호출)
+		/// 게임 초기화 함수 (본인 Main Scene 진입 시 호출)
 		/// </summary>
-		public virtual void InitializeGame()
+		public override void InitializeMain()
 		{
-			//base.InitializeGame();
+			//base.InitializeMain();
 		}
 		/// <summary>
-		/// 게임 마무리화 함수 (Game Scene 나갈 시 호출)
+		/// 게임 마무리화 함수 (본인 Main Scene 나갈 시 호출)
 		/// </summary>
-		public virtual void FinallizeGame()
+		public override void FinallizeMain()
 		{
-			base.FinallizeGame();
+			base.FinallizeMain();
 		}
+		#endregion
 	}
 }
