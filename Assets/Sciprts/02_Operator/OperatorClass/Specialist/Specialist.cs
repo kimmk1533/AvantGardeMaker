ead0646b64515039ff693432d7767f86146a2f52
JangHan Kim <kimjh741963@hanmail.net>
2025-05-04 21:35:26 +0900

    네임스페이스 누락된스크립트 수정

--- Assets/Sciprts/02_Operator/OperatorClass/Specialist/Specialist.cs
+++ Assets/Sciprts/02_Operator/OperatorClass/Specialist/Specialist.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-namespace AvantGardeMaker
+namespace AvantGardeMaker.OperatorSpace
 {
 	public class Specialist : SerializedMonoBehaviour
 	{
